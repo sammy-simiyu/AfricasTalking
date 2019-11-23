89fa3566c2ce4ce0d0613f37c84a70aef6fcca31
Sammy  W. Simiyu <swsimiyu@telkom.co.ke>
2019-11-23 11:42:23 +0300

    Added AT class

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
 <?php
-echo "AfricasTalking App";
-?>
+require 'vendor/autoload.php';
+use AfricasTalking\SDK\AfricasTalking;
+
+// Set your app credentials
+$username   = "MyAppsUsername";
+$apiKey     = "MyAppAPIKey";
+
+// Initialize the SDK
+$AT         = new AfricasTalking($username, $apiKey);
+
+// Get the SMS service
+$sms        = $AT->sms();
+
+// Set the numbers you want to send to in international format
+$recipients = "+254711XXXYYY,+254733YYYZZZ";
+
+// Set your message
+$message    = "I'm a lumberjack and its ok, I sleep all night and I work all day";
+
+// Set your shortCode or senderId
+$from       = "myShortCode or mySenderId";
+
+try {
+    // Thats it, hit send and we'll take care of the rest
+    $result = $sms->send([
+        'to'      => $recipients,
+        'message' => $message,
+        'from'    => $from
+    ]);
+
+    print_r($result);
+} catch (Exception $e) {
+    echo "Error: ".$e->getMessage();
+}
